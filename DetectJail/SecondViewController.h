de633dffe55ae05044634c95be1e38808241b2d5
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-12-12 19:47:38 +0300

    first commit

---
+++ DetectJail/SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  DetectJail
+//
+//  Created by Atil Samancioglu on 11.12.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DetectJail/SecondViewController.m
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.m
+//  DetectJail
+//
+//  Created by Atil Samancioglu on 11.12.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
